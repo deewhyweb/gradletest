cf6dcad076f5ccd9f74275ecf641898fcecd3680
Philip Hayes <phayes@PHAYES.local>
2014-08-05 16:05:53 +0100

    Added simple test

--- app/src/androidTest/java/com/feedhenry/ps/secondapp/ExampleTest.java
+++ app/src/androidTest/java/com/feedhenry/ps/secondapp/ExampleTest.java
@@ -7,7 +7,7 @@ import android.test.InstrumentationTestCase;
  */
 public class ExampleTest extends InstrumentationTestCase {
     public void test() throws Exception {
-        final int expected = 5;
+        final int expected = 2;
         final int reality = 5;
         assertEquals(expected, reality);
     }
